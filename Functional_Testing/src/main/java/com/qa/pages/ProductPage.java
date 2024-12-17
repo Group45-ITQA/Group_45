7a59ffba4224882572d1b308649640514039a792
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2024-12-18 00:26:08 +0530

    implement add multiple products to cart test

--- Functional_Testing/src/main/java/com/qa/pages/ProductPage.java
+++ Functional_Testing/src/main/java/com/qa/pages/ProductPage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.qa.utils.DriverManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductPage {
     private WebDriver driver;
 
@@ -18,19 +21,51 @@ public class ProductPage {
     @FindBy(id = "remove-sauce-labs-backpack")
     private WebElement removeButton;
 
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    private WebElement backpack;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    private WebElement bikeLight;
+
+    @FindBy(css = "[id^='remove']")
+    private List<WebElement> removeButtons;
+
+    private List<WebElement> workingProducts;
+
     // Shopping cart badge that shows number of items
     @FindBy(className = "shopping_cart_badge")
     private WebElement cartBadge;
 
+
+
     public ProductPage() {
         this.driver = DriverManager.getDriver();
         PageFactory.initElements(driver, this);
+        initializeWorkingProducts();
+    }
+
+    private void initializeWorkingProducts() {
+        workingProducts = new ArrayList<>();
+        workingProducts.add(backpack);
+        workingProducts.add(bikeLight);
     }
 
     // Adds the product to cart
     public void addProductToCart() {
         addToCartButton.click();
         sleep(1); // Wait for cart update
+    }
+
+    public void addMultipleProductsToCart(int numberOfProducts) {
+        int productsToAdd = Math.min(numberOfProducts, workingProducts.size());
+
+        for (int i = 0; i < productsToAdd; i++) {
+            WebElement product = workingProducts.get(i);
+            if (product != null && product.isEnabled()) {
+                product.click();
+                waitForCartUpdate();
+            }
+        }
     }
 
     // Verifies if the product was successfully added to cart
@@ -42,12 +77,25 @@ public class ProductPage {
         }
     }
 
+
+    public boolean areProductsAddedToCart(int expectedCount) {
+        return removeButtons.size() == expectedCount;
+    }
+
     // Gets the cart count (though this might not be the best verification)
     public String getCartCount() {
         try {
             return cartBadge.getText();
         } catch (Exception e) {
             return "0"; // Return "0" if no badge is found
+        }
+    }
+
+    private void waitForCartUpdate() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
