3e870c930082f37a9bd20e1dcfcfa17137137902
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2024-12-20 14:26:40 +0530

    Integrate allure

--- API_Testing/src/test/java/com/qa/steps/GetAllBooksSteps.java
+++ API_Testing/src/test/java/com/qa/steps/GetAllBooksSteps.java
@@ -4,60 +4,46 @@ import io.cucumber.java.en.*;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
-import java.util.List;
-import java.util.Map;
+import io.qameta.allure.*;
 
 import static io.restassured.RestAssured.given;
 import static com.qa.config.TestConfig.*;
 
+@Epic("Library API Testing")
+@Feature("Get Books API")
 public class GetAllBooksSteps {
     private RequestSpecification request;
     private Response response;
 
-
+    @Step("Setting up admin authentication")
     @Given("I am authenticated as an admin user with credentials")
     public void setupAdminAuthentication() {
-        // Setting up authentication - example for other team members
         request = given()
                 .auth()
                 .basic(ADMIN_USERNAME, PASSWORD)
                 .contentType("application/json");
     }
 
+    @Step("Sending GET request to fetch all books")
     @When("I send a request to get all books from the library")
     public void getAllBooks() {
-        // Making the API request - example of using BOOKS_ENDPOINT
         response = request
                 .when()
                 .get(BOOKS_ENDPOINT);
+
+        // Only attach response body
+        if (response != null && response.getBody() != null) {
+            Allure.addAttachment("Response Body", response.getBody().asString());
+        }
     }
 
+    @Step("Verifying response status code is {expectedStatusCode}")
     @Then("the API response status code should be {int}")
     public void verifyResponseStatusCode(int expectedStatusCode) {
-        // Verifying response code - common for all API tests
         Assert.assertEquals(
                 response.getStatusCode(),
                 expectedStatusCode,
                 "Expected status code " + expectedStatusCode + " but got " + response.getStatusCode()
         );
-    }
-
-    @And("the response should contain a list of all books")
-    public void verifyResponseContainsList() {
-        // Example of response validation - useful for GET and POST responses
-        List<Map<String, Object>> books = response.jsonPath().getList("$");
-        Assert.assertNotNull(books, "Response should contain a list of books");
-    }
-
-    @And("each book in the response should have the required fields")
-    public void verifyBookFields() {
-        // Example of detailed response validation - pattern for other tests
-        List<Map<String, Object>> books = response.jsonPath().getList("$");
-
-        books.forEach(book -> {
-            Assert.assertNotNull(book.get("id"), "Book must have an ID");
-            Assert.assertNotNull(book.get("title"), "Book must have a title");
-            Assert.assertNotNull(book.get("author"), "Book must have an author");
-        });
     }
 }
--- API_Testing/src/test/resources/features/GetAllBooks.feature
+++ API_Testing/src/test/resources/features/GetAllBooks.feature
@@ -1,12 +1,13 @@
+@allure.label.layer:api
+@allure.label.owner:malithi
 Feature: Get All Books API
   As a library administrator
   I want to retrieve all books in the system
   So that I can view the complete library catalog
 
-  @GetAllBooks @Smoke
-  Scenario: Successfully retrieve all books as admin user
+  @GetAllBooks
+  @allure.severity:critical
+  Scenario: Successfully retrieve all books via GET endpoint
     Given I am authenticated as an admin user with credentials
     When I send a request to get all books from the library
     Then the API response status code should be 200
-    And the response should contain a list of all books
-    And each book in the response should have the required fields
