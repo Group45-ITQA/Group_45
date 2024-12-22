69bb253c463928303fed01a9c3a25715325a9da5
Shamly-Shanawaz <m.shamlyshanawaz@gmail.com>
2024-12-22 20:15:55 +0530

    integrate allure

---
+++ Functional_Testing/src/test/java/com/qa/utils/AllureReportUtil.java
@@ -0,0 +1,14 @@
+package com.qa.utils;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class AllureReportUtil {
+
+    @Attachment(value = "Screenshot on failure", type = "image/png")
+    public static byte[] takeScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
