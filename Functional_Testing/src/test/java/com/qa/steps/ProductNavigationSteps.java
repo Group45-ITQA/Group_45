849650c21a4dcc28f544ea49e1e635d085cf0fd3
Shamly-Shanawaz <m.shamlyshanawaz@gmail.com>
2024-12-24 00:59:30 +0530

    implement product redirect check

---
+++ Functional_Testing/src/test/java/com/qa/steps/ProductNavigationSteps.java
@@ -0,0 +1,29 @@
+package com.qa.steps;
+
+import com.qa.pages.ProductPage;
+import io.cucumber.java.en.*;
+import io.qameta.allure.*;
+import org.testng.Assert;
+
+@Epic("Product Features")
+@Feature("Product Details Navigation")
+public class ProductNavigationSteps {
+    private ProductPage productPage;
+
+    public ProductNavigationSteps() {
+        productPage = new ProductPage();
+    }
+
+    @When("I click on the {string} product link")
+    @Step("Clicking on product link: {productName}")
+    public void clickProductLink(String productName) {
+        productPage.clickProductLink(productName);
+    }
+
+    @Then("I should see the correct product details for {string}")
+    @Step("Verifying correct product details are displayed for: {productName}")
+    public void verifyCorrectProductDetails(String productName) {
+        Assert.assertTrue(productPage.isCorrectProductDisplayed(productName),
+                "Defect: Wrong product details displayed after clicking " + productName);
+    }
+}
---
+++ Functional_Testing/src/test/resources/features/product_navigation.feature
@@ -0,0 +1,13 @@
+@product
+Feature: Product Details Navigation
+  As a user
+  I want to click on product links
+  So that I can view the correct product details
+
+  Background:
+    Given I am logged in and on the products page for product navigation
+
+  @product-navigation @defect @medium
+  Scenario: Verify product link redirects to correct product
+    When I click on the "Sauce Labs Backpack" product link
+    Then I should see the correct product details for "Sauce Labs Backpack"
