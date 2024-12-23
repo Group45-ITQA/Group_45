ad1ff74db8baea2c91279463c5bfe5fb60dc501a
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2024-12-23 12:08:55 +0530

    Update error

--- Functional_Testing/src/test/java/com/qa/steps/MultipleProductCartSteps.java
+++ Functional_Testing/src/test/java/com/qa/steps/MultipleProductCartSteps.java
@@ -18,7 +18,7 @@ public class MultipleProductCartSteps {
         loginPage = new LoginPage();
     }
 
-    @Given("I am logged in on the products page for multiple products\n")
+    @Given("I am logged in on the products page for multiple products")
     public void i_am_logged_in_on_products_page() {
         loginPage.login();
     }
