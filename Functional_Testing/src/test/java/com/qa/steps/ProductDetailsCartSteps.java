73e0403faf5e1304226ede322f1b8adc293f5d40
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2025-01-01 15:27:28 +0530

    implement detail product add to cart tests and refactor add multiple items to cart code

--- Functional_Testing/src/test/java/com/qa/steps/ProductDetailsCartSteps.java
+++ Functional_Testing/src/test/java/com/qa/steps/ProductDetailsCartSteps.java
@@ -1,57 +1,37 @@
 package com.qa.steps;
 
-import com.qa.pages.LoginPage;
-import com.qa.pages.ProductPage;
 import com.qa.pages.ProductDetailsPage;
 import io.cucumber.java.en.*;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import io.qameta.allure.*;
 import org.testng.Assert;
-import com.qa.utils.DriverManager;
 
+@Epic("Product Features")
+@Feature("Product Details Cart Integration")
+@Story("Add to Cart from Product Details")
 public class ProductDetailsCartSteps {
-    private ProductPage productPage;
-    private LoginPage loginPage;
-    private ProductDetailsPage productDetailsPage;
+    private final ProductDetailsPage productDetailsPage;
 
-    @Before
-    public void setup() {
-        productPage = new ProductPage();
-        loginPage = new LoginPage();
+    public ProductDetailsCartSteps() {
         productDetailsPage = new ProductDetailsPage();
     }
 
-    @And("I am redirected to the product details page")
-    public void i_am_redirected_to_product_details() {
-        Assert.assertTrue(productDetailsPage.isProductDetailsPageDisplayed(),
-                "Product details page is not displayed");
-    }
-
     @And("I click the Add to Cart button")
+    @Step("Clicking Add to Cart button on product details page")
     public void i_click_add_to_cart_button() {
         productDetailsPage.clickAddToCartButton();
     }
 
-    @Then("the product should be added to the cart")
-    public void verify_product_added_to_cart() {
-        Assert.assertTrue(productDetailsPage.isProductAddedToCart(),
-                "Product was not added to cart successfully");
-    }
-
-    @And("I should see the cart count increase to {string}")
+    @Then("I should see the cart count increase to {string}")
+    @Step("Verifying cart count is {expectedCount}")
     public void verify_cart_count(String expectedCount) {
         Assert.assertEquals(productDetailsPage.getCartCount(), expectedCount,
                 "Cart count does not match expected value");
     }
 
     @And("the Add to Cart button should change to Remove")
+    @Step("Verifying Add to Cart button changed to Remove")
     public void verify_button_changed_to_remove() {
         Assert.assertTrue(productDetailsPage.isRemoveButtonDisplayed(),
                 "Add to Cart button did not change to Remove");
-    }
-
-    @After
-    public void cleanup() {
-        DriverManager.quitDriver();
     }
 }
--- Functional_Testing/src/test/resources/features/product_details_add_to_cart.feature
+++ Functional_Testing/src/test/resources/features/product_details_add_to_cart.feature
@@ -1,13 +1,15 @@
+@product
 Feature: Product Details Add to Cart Functionality
   As a user
   I want to add products to cart from the product details page
   So that I can purchase items after viewing their details
 
-  Scenario: Add product to cart from product details page
+  Background:
     Given I am logged in and on the products page
-    When I click on a product name "Sauce Labs Backpack"
-    And I am redirected to the product details page
+
+  @product-details @add_to_cart @critical
+  Scenario: Add product to cart from product details page
+    When I click on the "Sauce Labs Backpack" product link
     And I click the Add to Cart button
-    Then the product should be added to the cart
-    And I should see the cart count increase to "1"
+    Then I should see the cart count increase to "1"
     And the Add to Cart button should change to Remove
