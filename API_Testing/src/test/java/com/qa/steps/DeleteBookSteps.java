d8ca508f9f487f39f2a4b2c98931b187b2380c09
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2025-01-03 00:27:13 +0530

    changed to use last book for delete

--- API_Testing/src/test/java/com/qa/steps/DeleteBookSteps.java
+++ API_Testing/src/test/java/com/qa/steps/DeleteBookSteps.java
@@ -2,30 +2,38 @@ package com.qa.steps;
 
 import io.cucumber.java.en.*;
 import io.qameta.allure.*;
-import com.qa.testdata.TestDataSetup;
-import org.testng.Assert;
+import io.restassured.response.Response;
+
+import java.util.List;
 
 import static com.qa.config.TestConfig.*;
+import static io.restassured.RestAssured.given;
 
 @Epic("LibraryAPITesting")
 @Feature("BookManagement")
 @Story("DeleteBook")
 public class DeleteBookSteps {
-    private Integer storedBookId;
-    private static final String TEST_BOOK_AUTHOR = "Test Author For Deletion";
+    private Integer lastBookId;
 
     @Given("I have a valid book ID to delete")
-    public void getValidBookId() {
-        storedBookId = TestDataSetup.getFirstBookId();
-        Assert.assertNotNull(storedBookId, "No test books found in database");
+    public void getLastBookId() {
+        Response getResponse = given()
+                .auth()
+                .basic(ADMIN_USERNAME, PASSWORD)
+                .when()
+                .get(BOOKS_ENDPOINT);
+
+        List<Integer> bookIds = getResponse.jsonPath().getList("id");
+        lastBookId = bookIds.get(bookIds.size() - 1);
     }
+
 
     @When("I send a DELETE request to remove the book with the stored ID")
     public void deleteBookWithStoredId() {
         BaseSteps.setResponse(
                 BaseSteps.getRequest()
                         .when()
-                        .delete(BOOKS_ENDPOINT + "/" + storedBookId)
+                        .delete(BOOKS_ENDPOINT + "/" + lastBookId)
         );
-    }
+}
 }
